7d5f441dc7b9e902ea345b3fd707f4d5be1ba1b3
Martin R. Pfaller <pfaller@stanford.edu>
2023-11-29 10:36:09 -0800

    test P2P1 in stokes/manufactured_solution

--- tests/test_stokes.py
+++ tests/test_stokes.py
@@ -10,7 +10,7 @@ base_folder = os.path.join("cases", "stokes")
 
 
 @pytest.mark.parametrize("mesh", ["N" + str(2**i).zfill(3) for i in range(2, 3)])
-@pytest.mark.parametrize("ele", ["P1P1"])
+@pytest.mark.parametrize("ele", ["P1P1", "P2P1"])
 def test_manufactured_solution(ele, mesh, n_proc):
     folder = os.path.join(base_folder, "manufactured_solution", ele, mesh)
     fields = ["Pressure", "Velocity"]
